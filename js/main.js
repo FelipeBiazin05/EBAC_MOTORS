35de78e30dc45f96b0cb2e10989dd519fb3d7c64
FelipeBiazin05 <142363711+FelipeBiazin05@users.noreply.github.com>
2024-04-16 20:56:56 -0300

    finalizando projeto

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ $(document).ready(function () {
     })
 
     $('#telefone').mask("(00) 00000-0000")
+    $('#cpf').mask("000.000.000-00")
+    $('#cep').mask("00000-000")
 
     $('form').validate({
         rules: {
@@ -26,6 +28,15 @@ $(document).ready(function () {
             },
             veiculoDeInteresse: {
                 required: false 
+            },
+            cpf: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
             }
         },
         messages: {
